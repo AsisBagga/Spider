9723c17461e34156a49e6665c5f0f52db5b5e57f
Asis Bagga <asisbagga@gmail.com>
2022-02-15 16:40:28 +0530

    Update test_views.py

--- DjangoWebCrawler/crawler/tests/test_views.py
+++ DjangoWebCrawler/crawler/tests/test_views.py
@@ -1,5 +1,4 @@
 from .test_setup import TestSetUp
-from crawler.models import CrawlerModel
 
 class TestViews(TestSetUp):
     def test_user_can_not_post_with_no_base_url(self):
@@ -10,22 +9,10 @@ class TestViews(TestSetUp):
         res = self.client.post(
             self.crawler_url, self.user_data, format="json"
         )
-        self.assertEqual(res.status_code, 201)
+        self.assertEqual(res.status_code, 200)
 
     def test_post_to_return_correct_data(self):
         res = self.client.post(
             self.crawler_url, self.user_data, format="json"
         )
-        self.assertEqual(res.data, "abc.com/xyz")
-    
-    def test_to_check_if_object_exists(self):
-        entry = CrawlerModel.objects.create(
-            base_url="abc.com/xyz",
-            site_links="abc.com"
-        )
-        entry.save()
-        res = self.client.post(
-            self.crawler_url, self.user_data, format="json"
-        )
-        self.assertEqual(res.data, "abc.com")
-        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.data, "abc.com/xyz\n")
